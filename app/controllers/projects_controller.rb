45981fe610b7b743f669f276400730d73050cc77
Ian Evans <icevans@icloud.com>
2019-08-28 14:27:13 -0600

    Set up projects index template

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,31 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :new, :edit, :create]
+
   def index
+    @projects = Project.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
   end
 end
