99d00f7d43ffe476e5ffac4590b1c8c4afea4ba6
Ian Evans <icevans@icloud.com>
2019-08-28 17:10:08 -0600

    Handle creating new ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -5,9 +5,29 @@ class TicketsController < ApplicationController
     @tickets = Ticket.all
   end
 
+  def create
+    ticket = Ticket.new(ticket_params)
+    ticket.project = Project.first # TODO: make dynamic
+
+    if ticket.save
+      flash[:notice] = 'Ticket created'
+      redirect_to tickets_path
+    else
+      render 'create'
+    end
+  end
+
+  def new
+    @ticket = Ticket.new
+  end
+
   private
 
   def set_ticket
     @ticket = Ticket.find(params[:id])
+  end
+
+  def ticket_params
+    params.require(:ticket).permit(:name, :body, :status)
   end
 end
