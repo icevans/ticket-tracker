b843591f0fc7633ae3a69cb1c4378d21ba3197cb
Ian Evans <icevans@icloud.com>
2019-08-29 15:27:03 -0600

    Handle user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:name, :email, :password))
+
+    if @user.save
+      flash[:notice] = "Welcome, #{@user.name}"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'projects#index'
 
+  get 'register' => 'users#new'
+
   resources :projects
   resources :tickets
   resources :tags, except: :show
+  resources :users, only: %i[create delete]
 end
