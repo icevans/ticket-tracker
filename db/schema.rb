00387bbcd1c080b5e0e315fa69d073f6a5bcc3f0
Ian Evans <icevans@icloud.com>
2019-08-29 12:42:42 -0600

    Create tagging model and migration
    This is the join table to represent the many-to
    many relation between tags and tickets.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_29_005343) do
+ActiveRecord::Schema.define(version: 2019_08_29_183430) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id", null: false
+    t.integer "ticket_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.index ["ticket_id"], name: "index_taggings_on_ticket_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -35,5 +44,7 @@ ActiveRecord::Schema.define(version: 2019_08_29_005343) do
     t.index ["project_id"], name: "index_tickets_on_project_id"
   end
 
+  add_foreign_key "taggings", "tags"
+  add_foreign_key "taggings", "tickets"
   add_foreign_key "tickets", "projects"
 end
