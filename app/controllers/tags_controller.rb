01ef7cee587788ec5f9f7fbe4084876588d7e23a
Ian Evans <icevans@icloud.com>
2019-08-29 12:53:56 -0600

    Handle creation of new tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,34 @@ class TagsController < ApplicationController
   def index
     @tags = Tag.all
   end
+
+  def show
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+
+    if @tag.save
+      flash[:notice] = 'Tag created'
+      redirect_to tags_path
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+  
+  def destroy
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
 end
