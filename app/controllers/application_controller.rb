b25c5c4c479fe193476158bd6a22298e17748e89
Ian Evans <icevans@icloud.com>
2019-08-30 08:18:23 -0600

    Add auth helper methods to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
-  def hello_world
+  helper_method :logged_in?, :current_user
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:error] = 'You must be logged in to do that'
+      redirect_to login_path
+    end
   end
 end
