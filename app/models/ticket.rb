721d548af0e523d0dbb4d7df177401ba3e5b03b0
Ian Evans <icevans@icloud.com>
2019-08-30 09:02:39 -0600

    Relate tickets and users via creator prop
    Also, show creator on ticket show view

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,6 @@
 class Ticket < ApplicationRecord
   belongs_to :project
+  belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
   has_many :taggings
   has_many :tags, through: :taggings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-
+  has_many :tickets, class_name: 'Ticket', foreign_key: 'creator_id'
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 end
