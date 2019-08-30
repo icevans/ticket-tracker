a85d92ab907e2144d899e01b2a9594a24c9279db
Ian Evans <icevans@icloud.com>
2019-08-30 10:30:23 -0600

    Relate users, tickets, and comments

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,6 +2,7 @@ class Ticket < ApplicationRecord
   belongs_to :project
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
   belongs_to :assignee, class_name: 'User', foreign_key: 'assignee_id'
+  has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :tickets, class_name: 'Ticket', foreign_key: 'creator_id'
   has_many :assigned_tickets, class_name: 'Ticket', foreign_key: 'assignee_id'
+  has_many :comments
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 end
