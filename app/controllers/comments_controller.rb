830421fa37eccda19a83821bf1ef7f11deadec9c
Ian Evans <icevans@icloud.com>
2019-08-30 11:16:04 -0600

    Handle creating new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+  def create
+    ticket = Ticket.find(params[:ticket_id])
+    comment = Comment.new(params.require(:comment).permit(:body))
+    comment.ticket = ticket
+    comment.creator = current_user
+
+    if comment.save
+      flash[:notice] = 'Comment created'
+      redirect_to ticket_path(ticket)
+    else
+      render 'tickets/show'
+    end
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   belongs_to :ticket
 end
