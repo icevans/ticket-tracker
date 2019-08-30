0562082bba30d5291759fa5ac69fad9398038ae0
Ian Evans <icevans@icloud.com>
2019-08-30 09:16:51 -0600

    Relate tickets and assignee
    Also show assignee on relevant ticket views

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,7 @@
 class Ticket < ApplicationRecord
   belongs_to :project
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
+  belongs_to :assignee, class_name: 'User', foreign_key: 'assignee_id'
   has_many :taggings
   has_many :tags, through: :taggings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :tickets, class_name: 'Ticket', foreign_key: 'creator_id'
+  has_many :assigned_tickets, class_name: 'Ticket', foreign_key: 'assignee_id'
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 end
