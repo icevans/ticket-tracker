0f1a32b8edfdb1b7829f1cb81f3704730d5fa086
Ian Evans <icevans@icloud.com>
2019-09-15 16:45:17 -0600

    Add dependent attributes
    For proper delete cascading on models

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,7 +2,7 @@ class Ticket < ApplicationRecord
   belongs_to :project
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
   belongs_to :assignee, class_name: 'User', foreign_key: 'assignee_id'
-  has_many :comments
-  has_many :taggings
+  has_many :comments, dependent: :destroy
+  has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
 end
